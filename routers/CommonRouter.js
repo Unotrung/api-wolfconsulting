e41defce0cd48e2660e52f430e2e57c27ccc6548
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-03-23 12:04:59 +0700

    Update All Project

--- routers/CommonRouter.js
+++ routers/CommonRouter.js
@@ -3,7 +3,7 @@ const MiddlewareController = require('../controllers/MiddlewareController');
 
 const router = require("express").Router();
 
-router.get("/generateContract", MiddlewareController.VerifyTokenByMySelf, CommonController.generateContract);
-router.put("/generateProviders", MiddlewareController.VerifyTokenByMySelf, CommonController.generateProviders);
+router.get("/generateContract", MiddlewareController.verifyToken, CommonController.generateContract);
+router.put("/generateProviders", MiddlewareController.verifyToken, CommonController.generateProviders);
 
 module.exports = router;
