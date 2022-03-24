6158a6c48e0e620bd2cf846b5390eb0704e82528
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-03-24 14:43:15 +0700

    Update API UPDATE EMAIL

--- controllers/MiddlewareController.js
+++ controllers/MiddlewareController.js
@@ -46,9 +46,7 @@ const middlewareController = {
         try {
             const token = req.body.token;
             if (token) {
-                // 'Beaer [token]'
-                const accessToken = token.split(" ")[1];
-                jwt.verify(accessToken, process.env.JWT_ACCESS_KEY, (err, user) => {
+                jwt.verify(token, process.env.JWT_ACCESS_KEY, (err, user) => {
                     if (err) {
                         return res.status(403).json("Token is not valid");
                     }
