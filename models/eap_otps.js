cf48def5bcd86b4bc795736d3582e3ed887c9ed3
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-03-24 11:22:59 +0700

    Update API EAP_OTPS

--- models/eap_otps.js
+++ models/eap_otps.js
@@ -3,18 +3,12 @@ const mongoose = require('mongoose');
 const eap_otpsSchema = new mongoose.Schema({
     username: {
         type: String,
-        minlength: [6, 'The minimum length of username is 6 characters'],
-        maxlength: [64, 'The maximum length of username is 64 characters'],
     },
     email: {
         type: String,
-        minlength: [10, 'The minimum length of email is 10 characters'],
-        maxlength: [255, 'The maximum length of email is 255 characters'],
     },
     phone: {
         type: String,
-        minlength: [10, 'Phone number must be 10 digits'],
-        maxlength: [10, 'Phone number must be 10 digits'],
     },
     otp: {
         type: String,
