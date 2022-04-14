cd1efffabb807169c3125505ef23eeab15d33c39
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-04-14 14:09:44 +0700

    Update API

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -7,18 +7,18 @@ const formatPassword = /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*#?&~^\-+_\(\)]{6,}$/;
 const errMessagePassword = 'Password must contain at least 1 uppercase letter and 1 number. The minimum password length is 6';
 const errMessageNewPassword = 'New password must contain at least 1 uppercase letter and 1 number. The minimum password length is 6';
 
-router.get("/", UserController.getAllUser);
+router.get("/", MiddlewareController.verifyToken, UserController.getAllUser);
 
 router.get("/:id", MiddlewareController.verifyTokenByMySelf, UserController.getUser);
 
 router.put("/:id",
-    [
-        check('username')
-            .isLength({ min: 3 }).withMessage('Minimum length of username is 3')
-            .isLength({ max: 255 }).withMessage('Maximum length of username is 255'),
-        check('password').matches(formatPassword).withMessage(errMessagePassword),
-        check('new_password').matches(formatPassword).withMessage(errMessageNewPassword),
-    ],
+    // [
+    //     check('username')
+    //         .isLength({ min: 3 }).withMessage('Minimum length of username is 3')
+    //         .isLength({ max: 255 }).withMessage('Maximum length of username is 255'),
+    //     check('password').matches(formatPassword).withMessage(errMessagePassword),
+    //     check('new_password').matches(formatPassword).withMessage(errMessageNewPassword),
+    // ],
     MiddlewareController.verifyTokenByMySelf, UserController.updateUser);
 
 module.exports = router;
