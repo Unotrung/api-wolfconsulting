dec91757996e3f39be0363525cf040d64453189a
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-04-18 23:35:33 +0700

    Update API generateRepayment generateTransaction

--- routers/CommonRouter.js
+++ routers/CommonRouter.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 
 router.get("/generateContract", CommonController.generateContract);
 router.get("/generateProviders", CommonController.generateProviders);
-router.get("/generateRepayment", CommonController.generateRepayment);
-router.get("/generateTransaction", CommonController.generateTransaction);
+router.post("/generateRepayment", CommonController.generateRepayment);
+router.post("/generateTransaction", CommonController.generateTransaction);
 
 module.exports = router;
