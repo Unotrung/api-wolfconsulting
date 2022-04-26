c41777a322b98a87974289b46f53782bf3d546a7
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-04-26 11:20:41 +0700

    Update

--- models/bnpl_providers.js
+++ models/bnpl_providers.js
@@ -1,17 +1,4 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-mongoose.connect(process.env.MONGODB_URL, function (err) {
-    if (!err) {
-        console.log('Connect MongoDB Successfully');
-    }
-    else {
-        console.log('Connect MongoDB Failure');
-    }
-}
-)
 
 const bnpl_providerSchema = new mongoose.Schema({
 
@@ -24,12 +11,4 @@ const bnpl_providerSchema = new mongoose.Schema({
 
 }, { timestamps: true });
 
-const bnpl_provider = mongoose.model('bnpl_provider', bnpl_providerSchema);
-for (let i = 1; i < 4; i++) {
-    bnpl_provider.create({
-        provider: `${i}`,
-        url: `${i}`
-    })
-}
-
-// module.exports = mongoose.model('bnpl_provider', bnpl_providerSchema);
+module.exports = mongoose.model('bnpl_provider', bnpl_providerSchema);
