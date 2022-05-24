15f1c243f245b2f81ec5602f84a61436a306da5f
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-05-24 10:20:06 +0700

    Update validate_data

--- config/validate_data/validate_data.js
+++ config/validate_data/validate_data.js
@@ -1,4 +1,4 @@
-const VALIDATE_PHONE = '/^(09|03|07|08|05)+([0-9]{8}$)/';
-const VALIDATE_PASSWORD = '/^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*#?&~^\-+_\(\)]{6,}$/';
+const VALIDATE_PHONE = /^(09|03|07|08|05)+([0-9]{8}$)/;
+const VALIDATE_PASSWORD = /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*#?&~^\-+_\(\)]{6,}$/;
 
 module.exports = { VALIDATE_PHONE, VALIDATE_PASSWORD }
